50d0c315a2faeaddd44976ad57ef76f35e845e38
mark-marushak <markk.9964@gmail.com>
2018-12-07 15:46:36 +0300

    http request

--- src/Transport/Curl.php
+++ src/Transport/Curl.php
@@ -140,7 +140,7 @@
             if (count($params) && $request_method == "GET") {
                 $query = http_build_query($params);
                 $query = preg_replace('/%5B[0-9]+%5D/simU', '%5B%5D', $query);
-                $url .= '/?' . $query;
+                $url .= '?' . $query;
             }
 
             if (!$token) {
